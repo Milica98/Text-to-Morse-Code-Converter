fd0fcb9f65b08a2b84158b380b8506436fbe0ccd
Milica98 <55503356+Milica98@users.noreply.github.com>
2024-01-08 22:39:15 +0100

    added light output of morse code

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def get_option():
 
 def get_output_type(input_text):
     return input(f"\nOriginal text: {input_text}\n"
-                 "--> To get text output type 'T' <--\n"
-                 "--> To get soud output type 'S' <--\n"
+                 "--> To get Text output type 'T' <--\n"
+                 "--> To get Sound output type 'S' <--\n"
+                 "--> To get Light output type 'L' <--\n"
                  "--> To finish current translation type anything else <--\n"
                  "Your option: ")
 
@@ -28,6 +29,39 @@ def sound_output(converted_text):
             winsound.Beep(frequency, duration_dash)
         elif c == ' ':
             time.sleep(duration_delay)
+
+def cursor_off():
+    print('\033[?25l', end="", flush=True)
+
+def cursor_on():
+    print('\033[?25h', end="", flush=True)
+
+def delete_characters(len):
+    while len > 0:
+        print('\b ', end='')
+        print('\b', end='')
+        len -= 1
+    print('', end='', flush=True)
+
+def light_on(time_duration):
+    print('\u2588' * 5, end='', flush=True)
+    time.sleep(time_duration)
+
+    delete_characters(5)
+    time.sleep(0.5)
+
+def light_output(converted_text):
+    cursor_off()
+    time.sleep(1)
+    for c in converted_text:
+        if c == '.':
+            light_on(duration_dot / 1000.0)
+        elif c == '-':
+            light_on(duration_dash / 1000.0)
+        elif c == ' ':
+            time.sleep(duration_delay)
+    time.sleep(1)
+    cursor_on()
 
 direction = get_option()
 
@@ -41,11 +75,13 @@ while direction in ['C', 'D']:
         else:
             output_type = get_output_type(input_text)
 
-            while output_type in ['T', 'S']:
+            while output_type in ['T', 'S', 'L']:
                 if output_type == 'T':
                     print(f"Coded text: {converted_text}")
                 elif output_type == 'S':
                     sound_output(converted_text)
+                elif output_type == 'L':
+                    light_output(converted_text)
                 input("Type Enter to continue...")
                 output_type = get_output_type(input_text)
 
